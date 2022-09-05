7883ebf2387a803e996cbb3392b6a04703c81e58
aliya2525 <108620523+aliya2525@users.noreply.github.com>
2022-09-05 17:23:02 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+resource "google_project" "my_test_project" {
+  name       = "my_test_project"
+  project_id = "252830"
+  }
+
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_test_project.252830
+  location_id = "us-central"
+}
