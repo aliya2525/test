0982e02315e9a0143a11ab0a526cda7cc3aef417
aliya2525 <108620523+aliya2525@users.noreply.github.com>
2022-09-06 14:48:32 +0530

    Update main.tf

--- appengine/main.tf
+++ appengine/main.tf
@@ -1,1 +1,29 @@
+resource "google_app_engine_standard_app_version" "myapp_v1" {
+  version_id = "v1"
+  service    = "myapp"
+  runtime    = "nodejs14"
 
+  entrypoint {
+    shell = "node ./app.js"
+  }
+
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.bucket.name}/${google_storage_bucket_object.object.name}"
+    }
+  }
+
+  env_variables = {
+    port = "8080"
+  }
+
+resource "google_storage_bucket" "bucket" {
+  name     = "appengine-static-content"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "hello-world.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./test-fixtures/appengine/hello-world.zip"
+}
